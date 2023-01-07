51faa35d4d9060f44bcf969f66c447ff9dbcf124
argenspin <86794565+argenspin@users.noreply.github.com>
2023-01-07 23:54:02 +0530

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2015 Agile Business Group
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Reports Custom Page",
+    "version": "1.0",
+    "author": "DataSoup",
+    "category": "Tools",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "data": [
+            "views/layouts.xml",
+            "views/res_company_views.xml",
+            "report/report_layout.xml",
+            ],
+    "depends": ["base","web","sale"],
+}
